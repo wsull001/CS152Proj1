ab5867054c04875b132f052b7de89c9cedcd349c
Ace Rhino <a12n@mac.com>
2017-04-29 17:57:59 -0700

    Began working on the LaTeX for our rules

---
+++ Language/rules_to_tex.cpp
@@ -0,0 +1,12 @@
+//Program to turn our rules (that are in c++) into valid LaTeX
+//Too lazy to use file i/o, so pipe the file into the program
+//-Stanley
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+}
