ff50424a580dd41ab08a50faddcc5ff706342680
Preston Giorgianni <pgior001@tango-41.cs.ucr.edu>
2017-05-26 09:45:22 -0700

    all new

--- README.md
+++
@@ -1,8 +0,0 @@
-# CS152Proj1
-### With Preston, Wyatt and now Stanley!
-testCases.txt -- add test programs here
-proj1.lex -- the source
-makefile -- just call make and it will compile
-running
-  ./a.out ---- command line editor
-  ./a.out [file name] --- compile program with file name
--- main.cc
+++ main.cc
@@ -1,85 +1,94 @@
-/* main.cc */
+// -*- c++ -*- 
+// main.cc
 
-#include "heading.h"
-#include "tok.h"
-#include <map>
+#include "heading.H"
+#include "nodes.h"
 
-// prototype of bison-generated parser function
-int yyparse();
+// Globals
+// int yylineno = 1;
+extern int yydebug;
+Program* root;     // pointer to root gets set by Program's constructor
+ostringstream decs;     // holds gen()-emitted target-code declarations
+ostringstream init;   // holds gen()-emitted intialization instructions
+ostringstream code;     // holds gen()-emitted target-code instructions
+string compilerName; // initialized by main() for use in error messages
+int yyparse();          // prototype of bison-generated parser function
 
-extern std::map<std::string, int> reserve;
-extern std::map<std::string, int> arithmetic;
-extern std::map<std::string, int> comparison;
-extern std::map<std::string, int> special;
-extern std::string exec_name;
+string preamble =
+   "// beginning of preamble  ================================\n\
+    using namespace std;                                      \n\
+    #include<string>            // Write out the preamble     \n\
+    #include<iostream>                                        \n\
+    // end of preamble and beginning of main declarations ====\n";
 
-void init_maps();
+string midamble =
+   "// end of declarations and beginning of midamble  ========\n\
+    main() {                    // main() of target code      \n";
 
-int main(int argc, char **argv)
-{
-  init_maps();
-  if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL))
-  {
-    cerr << argv[0] << ": File " << argv[1] << " cannot be opened.\n";
-    exit( 1 );
+string postamble =
+   "// end of main code and beginning of postamble ===========\n\
+    HALT:;                      // to stop                    \n\
+    } // end of main            // end of postamble           \n";
+
+
+
+int main( int argc, char* argv[] ) {  
+  // yydebug = 1;          // uncomment to enable parser debugging
+  // Allow for command-line specification of MiniJava source file
+  if ( argc > 1  &&  freopen( argv[1], "r", stdin) == NULL ) {
+    cerr << argv[0] << ": file " << argv[1]; 
+    cerr << " cannot be opened.\n";
+    exit( 1 ); 
   }
-  
-  exec_name = argv[0];
-  yyparse();
 
-  return 0;
+  compilerName = argv[0];           // For use in error messages
+  yyparse();                                // build syntax tree
+
+}
+
+// These should create a name and install it into the appropriate table.
+// It's attributes can be filled in later.
+
+string newVar()   {
+  static int counter = 0;
+  return "Var" + itoa(counter++);
+}
+
+string newArry()  {
+  static int counter = 0;
+  return "Arry" + itoa(counter++);
+}
+
+string newParam() {
+  static int counter = 0;
+  return "Param" + itoa(counter++);
+}
+
+string newFunct() {
+  static int counter = 0;
+  return "Funct" + itoa(counter++);
+}
+
+string newTemp()  {
+  static int counter = 0;
+  return "Temp" + itoa(counter++);
+}
+
+string newLabel() {
+  static int counter = 0;
+  return "Label" + itoa(counter++);
 }
 
 
-void init_maps(){
-  //reserve map init
-  reserve.insert(std::make_pair<std::string, int>("function", FUNCTION));
-  reserve.insert(std::make_pair<std::string, int>("beginparams", BEGIN_PARAMS));
-  reserve.insert(std::make_pair<std::string, int>("endparams", END_PARAMS));
-  reserve.insert(std::make_pair<std::string, int>("beginlocals", BEGIN_LOCALS));
-  reserve.insert(std::make_pair<std::string, int>("endlocals", END_LOCALS));
-  reserve.insert(std::make_pair<std::string, int>("beginbody", BEGIN_BODY));
-  reserve.insert(std::make_pair<std::string, int>("endbody", END_BODY));
-  reserve.insert(std::make_pair<std::string, int>("integer", INTEGER));
-  reserve.insert(std::make_pair<std::string, int>("array", ARRAY));
-  reserve.insert(std::make_pair<std::string, int>("of", OF));
-  reserve.insert(std::make_pair<std::string, int>("if", IF));
-  reserve.insert(std::make_pair<std::string, int>("then", THEN));
-  reserve.insert(std::make_pair<std::string, int>("endif", ENDIF));
-  reserve.insert(std::make_pair<std::string, int>("else", ELSE));
-  reserve.insert(std::make_pair<std::string, int>("while", WHILE));
-  reserve.insert(std::make_pair<std::string, int>("do", DO));
-  reserve.insert(std::make_pair<std::string, int>("beginloop", BEGINLOOP));
-  reserve.insert(std::make_pair<std::string, int>("endloop", ENDLOOP));
-  reserve.insert(std::make_pair<std::string, int>("continue", CONTINUE));
-  reserve.insert(std::make_pair<std::string, int>("read", READ));
-  reserve.insert(std::make_pair<std::string, int>("write", WRITE));
-  reserve.insert(std::make_pair<std::string, int>("and", AND));
-  reserve.insert(std::make_pair<std::string, int>("or", OR));
-  reserve.insert(std::make_pair<std::string, int>("not", NOT));
-  reserve.insert(std::make_pair<std::string, int>("true", TRUE));
-  reserve.insert(std::make_pair<std::string, int>("false", FALSE));
-  reserve.insert(std::make_pair<std::string, int>("return", RETURN));
-  //arithmetic map init
-  arithmetic.insert(std::make_pair<std::string, int>("-",SUB));
-  arithmetic.insert(std::make_pair<std::string, int>("+",ADD));
-  arithmetic.insert(std::make_pair<std::string, int>("*",MULT));
-  arithmetic.insert(std::make_pair<std::string, int>("/",DIV));
-  arithmetic.insert(std::make_pair<std::string, int>("%",MOD));
-  //comparison map init
-  comparison.insert(std::make_pair<std::string, int>("==",EQ));
-  comparison.insert(std::make_pair<std::string, int>("<>",NEQ));
-  comparison.insert(std::make_pair<std::string, int>("<",LT));
-  comparison.insert(std::make_pair<std::string, int>(">",GT));
-  comparison.insert(std::make_pair<std::string, int>("<=",LTE));
-  comparison.insert(std::make_pair<std::string, int>(">=",GTE));
-  //special map init
-  special.insert(std::make_pair<std::string, int>(";",SEMICOLON));
-  special.insert(std::make_pair<std::string, int>(":",COLON));
-  special.insert(std::make_pair<std::string, int>(",",COMMA));
-  special.insert(std::make_pair<std::string, int>("(",L_PAREN));
-  special.insert(std::make_pair<std::string, int>(")",R_PAREN));
-  special.insert(std::make_pair<std::string, int>("[",L_SQUARE_BRACKET));
-  special.insert(std::make_pair<std::string, int>("]",R_SQUARE_BRACKET));
-  special.insert(std::make_pair<std::string, int>(":=",ASSIGN));
-}
+  // The rest is left over from my MiniJava compiler  THP
+  // root->install();            // installation pass through Decls
+  // root->analyze();                              // analysis pass
+  // root->gen();                           // code generation pass
+  // cout << preamble;
+  // cout << decs.str();            // Write out the dec(laration)s
+  // cout << midamble;
+  // cout << init.str();    // Write out code to initialize vtables
+  // cout << code.str();                      // Write out the code
+  // cout << postamble;
+
+
