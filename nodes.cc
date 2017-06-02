5a84dd95caa7c1dcb04bade3543904df667d5b88
Preston Giorgianni <pgior001@tango-41.cs.ucr.edu>
2017-06-02 11:30:14 -0700

    newline changes

--- nodes.cc
+++ nodes.cc
@@ -9,10 +9,10 @@ Expression::Expression(Var* c1) {
 ReadStmt::ReadStmt( int c1, list<Var*>* c2 ) {
   for(auto var : *c2){
     if(var->index == ""){
-      this->code << ".< " + var->val + '\n';
+      this->code << ".< " + var->val <<endl;
     }
     else{
-      this->code << "[].< " + var->val + ", " + var->index + '\n';
+      this->code << "[].< " + var->val + ", " + var->index <<endl;
     }
   }
 }
@@ -21,19 +21,19 @@ ReadStmt::ReadStmt( int c1, list<Var*>* c2 ) {
 WriteStmt::WriteStmt( int c1, list<Var*>* c2 ) {
   for(auto var : *c2){
     if(var->index == ""){
-      this->code << ".> " + var->val + '\n';
+      this->code << ".> " + var->val <<endl;
     }
     else{
-      this->code << "[].> " + var->val + ", " + var->index;
+      this->code << "[].> " + var->val + ", " + var->index << endl;
     }
   }
 }
 
 AssignmentStmt::AssignmentStmt( Var* c1, int c2, Expression* c3 ) {
   if(c1->index != ""){
-    code << "=[] " + c1->val + ", " + c1->index + '\n';
+    code << "=[] " + c1->val + ", " + c1->index << endl;
   } else {
-    code << "= " + c1->val + ", " + c3->val; 
+    code << "= " + c1->val + ", " + c3->val << endl; 
   }
 }
 
