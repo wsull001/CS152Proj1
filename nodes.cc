7e4523259f21c66f1b0bd78f7184d667e2342c05
Preston Giorgianni <pgior001@tango-41.cs.ucr.edu>
2017-06-02 11:58:44 -0700

    if stm working

--- nodes.cc
+++ nodes.cc
@@ -41,7 +41,9 @@ AssignmentStmt::AssignmentStmt( Var* c1, int c2, Expression* c3 ) {
 IfThenStmt::IfThenStmt( int c1, BoolExpr* c2, int c3, Statements* c4, int c5 ) {
     after = newLabel();
     code << c2->code.str();
-    code << "?:= " << after << ", " << c2->val << endl;
+    string temp = newTemp(code);
+    code << "! " << temp << ", " << c2->val << endl;
+    code << "?:= " << after << ", " << temp << endl;
     for(auto st: *c4){
       code << st->code.str();
     }
